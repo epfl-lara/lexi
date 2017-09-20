784970e457b72fae277f7965b2edcb2503d21802
Georg Schmid <georg.schmid@epfl.ch>
2017-09-20 11:30:36 +0200

    Cleanup of some alternative implementations.

--- src/main/scala/lexi/Automaton.scala
+++ src/main/scala/lexi/Automaton.scala
@@ -32,23 +32,6 @@ case class DFA(states: List[State], alphabet: Set[Sym],
 {
   Automaton.checkValid(states, alphabet, trans, initState, finalStates)
 
-//  digraph finite_state_machine {
-//    rankdir=LR;
-//    size="8,5"
-//
-//    node [shape = doublecircle]; S;
-//    node [shape = point ]; qi
-//
-//    node [shape = circle];
-//    qi -> S;
-//    S  -> q1 [ label = "a" ];
-//    S  -> S  [ label = "a" ];
-//    q1 -> S  [ label = "a" ];
-//    q1 -> q2 [ label = "b" ];
-//    q2 -> q1 [ label = "b" ];
-//    q2 -> q2 [ label = "b" ];
-//  }
-
   def toGraphViz(): String = {
     def stateName(state: State) = s"S${state.id}"
     val nodeDecls = finalStates.map(s => s"node [shape = doublecircle]; ${stateName(s)}")
@@ -67,28 +50,10 @@ case class DFA(states: List[State], alphabet: Set[Sym],
      """.stripMargin
   }
 
-//  def withoutUnreachable(): DFA = {
-//    @tailrec def closure(ss: Set[State]): Set[State] = {
-//      val ss1 = alphabet.flatMap(c => ss.collect ... )
-//      if (ss.size == ss1.size) ss else closure(ss1)
-//    }
-//    val newStates = closure(Set(initState))
-//    DFA(newStates.toList, alphabet, trans.filter { case ((s0, _), _) => newStates.contains(s0) },
-//      initState, finalStates.toSet.intersect(newStates).toList)
-//  }
-
   def minimized(): DFA = {
     val finalStatesSet = finalStates.toSet
     val partition = mutable.ArrayBuffer[Set[State]](finalStatesSet, states.toSet diff finalStatesSet)
 
-//    val invTrans = {
-//      val acc = mutable.Map[(State, Sym), Set[State]]()
-//      trans.foreach { case ((s0, c), s1) =>
-//        val key = (s1, c)
-//        acc.update(key, acc.getOrElse(key, Set.empty) + s0)
-//      }
-//      acc.toMap
-//    }
     val invTrans = {
       for { ((s0, c), s1) <- trans.view }
         yield ((s1, c), s0)
@@ -259,51 +224,8 @@ object NFA {
       }
     }
 
-//    def transform(stateIn: State, regex: Regex)(ctx: (State, Boolean, Set[State]) => Set[State]): Set[State] = {
-//      import Regex._
-//      lazy val stateOut = freshState()
-//      regex match {
-//        case Epsilon =>
-//          addEdge(stateIn, EpsilonSym, stateOut)
-//          ctx(stateOut, true, Set(stateOut))
-//
-//        case Element(syms) =>
-//          assert(!syms.contains(EpsilonSym))
-//          syms.foreach(addEdge(stateIn, _, stateOut))
-//          ctx(stateOut, false, Set(stateOut))
-//
-//        case Concat(r1, r2) =>
-//          transform(stateIn, r1) { (out1, maybeEmpty1, finals1) =>
-//            transform(out1, r2) { (out2, maybeEmpty2, finals2) =>
-//              ctx(out2, maybeEmpty1 && maybeEmpty2, if (maybeEmpty2) finals1 union finals2 else finals2)
-//            }
-//          }
-//
-//        case Alt(r1, r2) =>
-//          val in1 = freshState()
-//          val in2 = freshState()
-//          addEdge(stateIn, EpsilonSym, in1)
-//          addEdge(stateIn, EpsilonSym, in2)
-//          transform(in1, r1) { (out1, maybeEmpty1, finals1) =>
-//            addEdge(out1, EpsilonSym, stateOut)
-//            transform(in2, r2) { (out2, maybeEmpty2, finals2) =>
-//              addEdge(out2, EpsilonSym, stateOut)
-//              ctx(stateOut, maybeEmpty1 || maybeEmpty2, finals1 union finals2)
-//            }
-//          }
-//
-//        case Star(r) =>
-//          addEdge(stateIn, EpsilonSym, stateOut)
-//          transform(stateOut, r) { (outSub, maybeEmpty, finals) =>
-//            addEdge(outSub, EpsilonSym, stateOut)
-//            ctx(stateOut, true, Set(stateOut))  // CHECKME: ?! finals + stateOut ?!
-//          }
-//      }
-//    }
-
     val initState = freshState()
     val finalStates = List[State](transform(initState, regex))
-//    val finalStates = transform(initState, regex){ (_, _, fs) => fs }.toList
     NFA(states, alphabet - EpsilonSym, edges.toMap, initState, finalStates)
   }
 }
